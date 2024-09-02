b09bec9d6c3fdde0c1fd3fc03be16e4575176ad4
Brauny74 <kazinfm@gmail.com>
2024-09-02 21:49:31 +0500

    Created the project

---
+++ Models/SeedData.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using L11.Data;
+using System;
+using System.Linq;
+
+namespace L11.Models
+{
+	public static class SeedData
+	{
+		public static void Initialize(IServiceProvider serviceProvider)
+		{
+			using (var context = new MvcMovieContext(
+				serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()
+				))
+			{
+				if (context.Movie.Any())
+					return;
+
+				context.Movie.AddRange(
+					new Movie
+					{ 
+						Title = "Когда Гарри встретил Салли",
+						ReleaseDate = DateTime.Parse("1989-2-12"),
+						Genre = "романтика",
+						Price = 450.0m
+					},
+					new Movie
+					{
+						Title = "Назад в будущее",
+						ReleaseDate = DateTime.Parse("1985-7-3"),
+						Genre = "фантастика",
+						Price = 500.0m
+					},
+					new Movie
+					{
+						Title = "Назад в будущее 2",
+						ReleaseDate = DateTime.Parse("1989-11-22"),
+						Genre = "фантастика",
+						Price = 500.0m
+					},
+					new Movie
+					{
+						Title = "Назад в будущее 3",
+						ReleaseDate = DateTime.Parse("1990-5-25"),
+						Genre = "фантастика",
+						Price = 500.0m
+					},
+					new Movie
+					{
+						Title = "Охотники за приведениями",
+						ReleaseDate = DateTime.Parse("1984-3-13"),
+						Genre = "комедия",
+						Price = 560.0m
+					},
+					new Movie
+					{
+						Title = "Охотники за приведениями 2",
+						ReleaseDate = DateTime.Parse("1986-2-23"),
+						Genre = "комедия",
+						Price = 560.0m
+					},
+					new Movie
+					{
+						Title = "Роки",
+						ReleaseDate = DateTime.Parse("1976-12-3"),
+						Genre = "спортивная драма",
+						Price = 470.0m
+					}
+					);
+				context.SaveChanges();
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+using L11.Data;
+using L11.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<MvcMovieContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            var scope = app.Services.CreateScope();
+            SeedData.Initialize(scope.ServiceProvider);
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
