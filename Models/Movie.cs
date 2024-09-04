fd621c76bbf5817470044003b311180359ab2fc3
Brauny74 <kazinfm@gmail.com>
2024-09-04 21:17:11 +0500

    Added Search functionality

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,12 +6,16 @@ namespace L11.Models
 	public class Movie
 	{
 		public int Id { get; set; }
+		[Display(Name = "Название")]
 		public string Title { get; set; }
-		
+
+		[Display(Name = "Дата выхода")]
 		[DataType(DataType.Date)] 
 		public DateTime ReleaseDate { get; set; }
 
+		[Display(Name = "Жанр")]
 		public string Genre { get; set; }
+		[Display(Name = "Цена")]
 		public decimal Price { get; set; }
 	}
 }
